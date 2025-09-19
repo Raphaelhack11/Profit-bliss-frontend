b793ecce57e87894521d1f20938c18547e317099
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-09-19 08:21:50 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        pb: {
+          900: "#08102A",
+          800: "#0B1B3A",
+          700: "#0F2A57"
+        }
+      }
+    }
+  },
+  plugins: []
+}
