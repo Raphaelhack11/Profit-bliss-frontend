7aee268e1d2fcc8e3f8e3d7ee2c67e552cf17d2b
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-09-19 08:23:04 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./authContext";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+        <Toaster position="top-right" reverseOrder={false} />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
