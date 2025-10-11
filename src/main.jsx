44c926e5d9ce9f086465398bceb2bc2829e8a488
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-10-11 10:20:49 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./index.css";
+import { AuthProvider } from "./authContext";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+        <Toaster position="top-right" reverseOrder={false} />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
