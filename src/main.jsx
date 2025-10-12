98d0398c9fbea7115a0bf8fd6347e189e534fbbe
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-10-12 11:09:37 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import DebugApp from "./DebugApp";
-import "./index.css";
 import { AuthProvider } from "./authContext";
 import { Toaster } from "react-hot-toast";
-import ErrorBoundary from "./components/ErrorBoundary"; // ✅ Add this line
+import DebugApp from "./DebugApp"; // ✅ temporary debug app
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ErrorBoundary> {/* ✅ Wrap entire app */}
-      <BrowserRouter>
-        <AuthProvider>
-          <App />
-          <Toaster position="top-right" reverseOrder={false} />
-        </AuthProvider>
-      </BrowserRouter>
-    </ErrorBoundary>
+    <BrowserRouter>
+      <AuthProvider>
+        <DebugApp />
+        <Toaster position="top-right" reverseOrder={false} />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
