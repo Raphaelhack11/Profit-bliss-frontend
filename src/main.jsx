351c2fb247e84fe55d05a2d164e47007a28a850a
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-10-12 11:16:30 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./authContext";
 import { Toaster } from "react-hot-toast";
-import DebugApp from "./DebugApp"; // ✅ temporary debug app
+import App from "./App"; // ✅ temporary debug app
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
