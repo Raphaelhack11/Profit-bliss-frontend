7e51044eef21182e64507744c1ba4b6949d0d76e
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-10-16 11:14:04 -0400

    Update AdminRoute.jsx

--- src/components/AdminRoute.jsx
+++ src/components/AdminRoute.jsx
@@ -2,20 +2,17 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const AdminRoute = ({ children }) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("pb_token");
   const role = localStorage.getItem("pb_role");
 
-  // No token = not logged in
   if (!token) {
     return <Navigate to="/login" replace />;
   }
 
-  // Not an admin = redirect to dashboard (or landing)
   if (role !== "admin") {
     return <Navigate to="/dashboard" replace />;
   }
 
-  // ✅ Authenticated admin — allow access
   return children;
 };
 
