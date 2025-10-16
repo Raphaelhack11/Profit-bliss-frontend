9c7ec76445bcfa57b0608f33ea54cb9af62a4076
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-10-16 10:03:12 -0400

    Refactor main.jsx to use ReactDOM and simplify structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import { AuthProvider } from "./authContext";
+import App from "./App.jsx";
 import "./index.css";
 
-const root = createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
